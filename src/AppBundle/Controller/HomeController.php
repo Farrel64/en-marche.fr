d6963500a99702352984550c921bbfe59510e66f
Titouan Galopin <galopintitouan@gmail.com>
2016-12-22 01:36:10 +0100

    Implement filesystem repositories to store articles as YAML and Markdown

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -14,6 +14,22 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('home/index.html.twig');
+        $repository = $this->get('app.filesystem.article_repository');
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $repository->getHomeArticles(),
+            'live_links' => $repository->getHomeLiveLinks(),
+        ]);
+    }
+
+    /**
+     * @Route("/article/{slug}", name="article_view")
+     * @Method("GET")
+     */
+    public function articleAction($slug)
+    {
+        return $this->render('home/index.html.twig', [
+            'article' => $this->get('app.filesystem.article_repository')->getArticle($slug),
+        ]);
     }
 }
